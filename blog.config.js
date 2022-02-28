fcb7e2f4b215990cfebda25335cc25bdde80f2e1
xiachongder <89074896+xiachongder@users.noreply.github.com>
2022-02-28 21:39:34 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,7 +1,7 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
+  title: 'xiachongderBlog',
+  author: 'xiachong',
+  email: '',
   link: 'https://nobelium.vercel.app',
   description: 'This gonna be an awesome website.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
